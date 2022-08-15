1b1d06a9f08481146f46a58e5d011d4f0e115f45
Ngô Tuấn <ngotuan@admins-MacBook-Air.local>
2022-08-15 18:32:55 +0700

    add confirm user management

--- src/pages/UserManagement/TableUserManagement.js
+++ src/pages/UserManagement/TableUserManagement.js
@@ -1,39 +1,43 @@
-import { Table } from "antd";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { columnsUserManagement } from "../../utils/userManagement";
-import { getListUser } from "../../redux/userSlice";
-import ModalUserManagement from "./EditInfoUser/ModalUserManagement";
-import ModalAddAdmin from "./AddAdmin/ModalAddAdmin.jsx";
+import { Table } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { columnsUserManagement } from '../../utils/userManagement';
+import { getListUser } from '../../redux/userSlice';
+import ModalUserManagement from './EditInfoUser/ModalUserManagement';
+import ModalAddAdmin from './AddAdmin/ModalAddAdmin.jsx';
 
 export default function TableUserManagement() {
-  const { dataListUser } = useSelector((state) => state.userSlice);
-  const [data, setData] = useState([]);
-  let dispatch = useDispatch();
+    const { dataListUser } = useSelector((state) => state.userSlice);
+    const { visible } = useSelector((state) => state.userSlice);
+    const [data, setData] = useState([]);
+    let dispatch = useDispatch();
 
-  useEffect(() => {
-    if (dataListUser) {
-      let arrNew = dataListUser.map((item) => {
-        return { ...item, action: dispatch };
-      });
-      setData(arrNew);
-    }
-  }, [dataListUser]);
+    useEffect(() => {
+        if (dataListUser) {
+            let arrNew = dataListUser.map((item) => {
+                return {
+                    ...item,
+                    action: { dispatch, visible },
+                };
+            });
+            setData(arrNew);
+        }
+    }, [dataListUser, visible]);
 
-  useEffect(() => {
-    dispatch(getListUser());
-  }, []);
+    useEffect(() => {
+        dispatch(getListUser());
+    }, []);
 
-  return (
-    <>
-      <Table
-        bordered
-        dataSource={data}
-        columns={columnsUserManagement}
-        rowKey={"_id"}
-      />
-      <ModalUserManagement />
-      <ModalAddAdmin />
-    </>
-  );
+    return (
+        <>
+            <Table
+                bordered
+                dataSource={data}
+                columns={columnsUserManagement}
+                rowKey={'_id'}
+            />
+            <ModalUserManagement />
+            <ModalAddAdmin />
+        </>
+    );
 }
--- src/utils/userManagement.js
+++ src/utils/userManagement.js
@@ -1,17 +1,19 @@
-import userPic from "../assets/img/user_pic.png";
+import { Button, Popover } from 'antd';
+import userPic from '../assets/img/user_pic.png';
 import {
     openCloseModalEdit,
     getInfoUser,
     getIdUserPut,
     deleteUser,
-} from "../redux/userSlice";
+    handleChangeVisible,
+} from '../redux/userSlice';
 
 export const columnsUserManagement = [
     {
-        title: "Tên",
-        dataIndex: "name",
-        key: "name",
-        align: "center",
+        title: 'Tên',
+        dataIndex: 'name',
+        key: 'name',
+        align: 'center',
         sorter: true,
         sorter: (a, b) => {
             if (a.name > b.name) {
@@ -25,11 +27,11 @@ export const columnsUserManagement = [
     },
 
     {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
-        align: "center",
-        className: "font-medium",
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+        align: 'center',
+        className: 'font-medium',
         sorter: (a, b) => {
             if (a.email > b.email) {
                 return 1;
@@ -41,10 +43,10 @@ export const columnsUserManagement = [
         },
     },
     {
-        title: "Địa chỉ",
-        dataIndex: "address",
-        key: "address",
-        align: "center",
+        title: 'Địa chỉ',
+        dataIndex: 'address',
+        key: 'address',
+        align: 'center',
         sorter: (a, b) => {
             if (a.address > b.address) {
                 return 1;
@@ -57,41 +59,82 @@ export const columnsUserManagement = [
     },
 
     {
-        title: "Số điện thoại",
-        dataIndex: "phone",
-        key: "phone",
-        align: "center",
+        title: 'Số điện thoại',
+        dataIndex: 'phone',
+        key: 'phone',
+        align: 'center',
     },
     {
-        title: "Mã loại",
-        dataIndex: "type",
-        key: "type",
-        align: "center",
+        title: 'Mã loại',
+        dataIndex: 'type',
+        key: 'type',
+        align: 'center',
     },
     {
-        title: "Thao tác",
-        dataIndex: "action",
-        key: "action",
-        align: "center",
-        render: (dispatch, record) => {
+        title: 'Thao tác',
+        dataIndex: 'action',
+        key: 'action',
+        align: 'center',
+        render: ({ dispatch, visible }, record) => {
             return (
-                <div className="flex justify-center space-x-4 w-full h-full">
+                <div className="flex justify-center w-full h-full space-x-4">
                     <button
-                        className="text-white bg-blue-600 px-4 py-2 rounded font-medium"
+                        className="px-4 py-2 font-medium text-white bg-blue-600 rounded"
                         onClick={() => (
                             dispatch(getInfoUser(record._id)),
                             dispatch(getIdUserPut(record._id)),
                             dispatch(openCloseModalEdit(true))
                         )}
+                        d
                     >
                         Sửa
                     </button>
-                    <button
-                        className="text-white bg-red-600 px-4 py-2 rounded font-medium"
-                        onClick={() => dispatch(deleteUser(record._id))}
+                    <Popover
+                        className="z-10"
+                        content={
+                            <div className="flex justify-end space-x-4">
+                                <button
+                                    className="px-4 py-2 font-medium text-blue-500 rounded hover:bg-gray-200"
+                                    onClick={() =>
+                                        dispatch(
+                                            handleChangeVisible({
+                                                id: null,
+                                                value: false,
+                                            })
+                                        )
+                                    }
+                                >
+                                    Huỷ
+                                </button>
+                                <button
+                                    className="px-4 py-2 font-medium text-white bg-blue-600 rounded"
+                                    onClick={() =>
+                                        dispatch(deleteUser(record._id))
+                                    }
+                                >
+                                    Đồng ý
+                                </button>
+                            </div>
+                        }
+                        placement="bottomLeft"
+                        title="Bạn có muốn xoá người dùng này không?"
+                        trigger="click"
+                        visible={
+                            visible.id === record._id ? visible.value : false
+                        }
+                        onVisibleChange={() => {
+                            dispatch(
+                                handleChangeVisible({
+                                    id: record._id,
+                                    value: true,
+                                })
+                            );
+                        }}
                     >
-                        Xóa
-                    </button>
+                        <button className="px-4 py-2 font-medium text-white bg-red-600 rounded">
+                            Xóa
+                        </button>
+                    </Popover>
                 </div>
             );
         },
