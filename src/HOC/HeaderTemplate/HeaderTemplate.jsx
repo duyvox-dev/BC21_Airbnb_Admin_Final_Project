debef27a78afa9b418fa0dfad4136c5790d3d62e
duyvox-dev <vobaoduy123@gmail.com>
2022-08-15 22:00:44 +0700

    increase header's zindex
    increase header's zindex

--- src/HOC/HeaderTemplate/HeaderTemplate.jsx
+++ src/HOC/HeaderTemplate/HeaderTemplate.jsx
@@ -4,14 +4,14 @@ import logo3 from "../../assets/img/airbnb-logo3.png";
 import MenuHeader from "./MenuHeader";
 
 export default function HeaderTemplate() {
-  return (
-    <div className="w-full h-20 shadow fixed top-0 z-10 bg-white">
-      <div className="w-10/12 h-full mx-auto flex justify-between items-center">
-        <NavLink to="/user">
-          <img className="w-40 h-12" src={logo3} alt={logo3} />
-        </NavLink>
-        <MenuHeader />
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full h-20 shadow fixed top-0 z-[999] bg-white">
+            <div className="w-10/12 h-full mx-auto flex justify-between items-center">
+                <NavLink to="/user">
+                    <img className="w-40 h-12" src={logo3} alt={logo3} />
+                </NavLink>
+                <MenuHeader />
+            </div>
+        </div>
+    );
 }
