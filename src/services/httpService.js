f9ab1db641242b89f6b663138117f73d327dd7d5
duyvox-dev <vobaoduy123@gmail.com>
2022-08-16 10:35:38 +0700

    add loading component
    add loading component

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,7 @@
-// import { store } from "../redux/store";
 import axios from "axios";
 import { getRequestConfig, getAccessToken } from "./configURL";
 import { startLoading, stopLoading } from "../redux/loadingSlice";
+import { store } from "../redux/store";
 export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 let timeRequestMax;
 
@@ -15,25 +15,25 @@ export const httpService = axios.create({
 //Action can thiệp trước khi gọi request API
 httpService.interceptors.request.use(
     function (config) {
-        // store.dispatch(startLoading());
+        store.dispatch(startLoading());
         const accessToken = getAccessToken();
         if (accessToken) config.headers.token = accessToken;
         else delete httpService.defaults.headers.common.token;
         return config;
     },
     function (error) {
-        // store.dispatch(stopLoading());
+        store.dispatch(stopLoading());
         return Promise.reject(error);
     }
 );
 //Action can thiệp sau khi có request API trả về
 httpService.interceptors.response.use(
     function (response) {
-        // store.dispatch(stopLoading());
+        store.dispatch(stopLoading());
         return response;
     },
     function (error) {
-        // store.dispatch(stopLoading());
+        store.dispatch(stopLoading());
         return Promise.reject(error);
     }
 );
